c167b954af530543fc2951fbc7cd931216fa946a
Oliver Berg <oliver@berg.moe>
2022-06-26 20:02:30 +0200

    Add more info

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,28 +10,19 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
+    mingwX64 {
         binaries {
             executable {
                 entryPoint = "main"
             }
         }
     }
+
     sourceSets {
-        val nativeMain by getting {
+        val mingwX64Main by getting {
             dependencies {
                 implementation("com.github.ajalt.clikt:clikt:3.5.0")
             }
         }
-        val nativeTest by getting
     }
 }
